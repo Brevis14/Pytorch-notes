0f50c3b387301b28497d87f77d05fc3f7ecaf225
Faye <xuhuihua1414@outlook.com>
2025-08-05 14:46:44 +0800

    Add files via upload

---
+++ 08-tensorboard_openImg.py
@@ -0,0 +1,23 @@
+import os
+
+import numpy as np
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+
+#创建SummaryWriter的类和对象
+writer = SummaryWriter("logs")
+print("Current working directory:", os.getcwd())
+
+#img_path = "dataset/exercise_data/train/ants_image/0013035.jpg"
+img_path = "dataset/exercise_data/train/bees_image/16838648_415acd9e3f.jpg"
+img_PIL = Image.open(img_path)
+img_array = np.array(img_PIL)
+print(type(img_array))
+print(img_array.shape)
+
+writer.add_image("test", img_array, 2, dataformats="HWC")
+# y = x
+for i in range(100):
+    writer.add_scalar("y = 2x", 3*i, i)
+
+writer.close()
